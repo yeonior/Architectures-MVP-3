2bdccf82e44f2ee6c8a56fafebbbf7d7f7a5aa36
Rus <de.mr44@gmail.com>
2022-02-11 23:48:34 +0500

    Initial Commit

---
+++ MVP-Storyboard-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Storyboard-3
+//
+//  Created by Ruslan on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVP-Storyboard-3UITests/MVP_Storyboard_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVP_Storyboard_3UITestsLaunchTests.swift
+//  MVP-Storyboard-3UITests
+//
+//  Created by Ruslan on 11.02.2022.
+//
+
+import XCTest
+
+class MVP_Storyboard_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
